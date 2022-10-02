dfe3ecf31936a2319f0b953189ba54ae7211962a
Andres <andres.quiroz@payclip.com>
2022-10-02 12:54:52 -0500

    fix: git remote

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,5 +6,5 @@ repository_name = repository_prefix+'{{ cookiecutter.project_name }}.git'
 
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
-subprocess.call(['git', 'remote', 'add', 'origin', 'repository_name'])
+subprocess.call(['git', 'remote', 'add', 'origin', repository_name])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
