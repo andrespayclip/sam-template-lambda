32ebfadf174c6561f72a5d6549d532263c511b7a
Andres <andres.quiroz@payclip.com>
2022-10-02 12:42:25 -0500

    update: add remote for repository

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,10 @@
 import subprocess
 
+repository_prefix = 'https://github.com/andrespayclip/'
+repository_name = repository_prefix+'{{ cookiecutter.project_name }}.git'
+
+
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'remote', 'add', 'origin', 'repository_name'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
